1de1c8d01b6356cf3438a975974ea2344904da3e
Philipp Wahle <wahle@thesisdigital.de>
2016-02-23 13:35:44 +0100

    Update gulp-sass dependencies, fix sourcemap issue. and split build:sass task into two seperate task

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,6 @@ var gulp = require('gulp'),
     del = require('del'),
     runSequence = require('run-sequence');
 
-
 var pkg = require('./package.json');
 var dirs = pkg['project-configs'].directories;
 var config = pkg['project-configs'].config;
@@ -101,6 +100,7 @@ gulp.task('lint:sass', function () {
 gulp.task('build', [
     'build:html',
     'build:sass',
+    'minify:sass',
     'build:js'
 ]);
 
@@ -144,15 +144,31 @@ gulp.task('build:js', function () {
  *   http://bourbon.io/
  */
 gulp.task('build:sass', function () {
-    var SRC = dirs.src + '/scss/*.s+(a|c)ss';
+    var SRC = dirs.src + '/scss/**/*.s+(a|c)ss';
     var DEST = dirs.dist + '/css/';
 
     return gulp.src(SRC)
-        .pipe(plugins.sassGlob())
         .pipe(plugins.sourcemaps.init())
+        .pipe(plugins.sassGlob())
+
+        ////filter out unchanged scss files, only works when watching
+        // .pipe(plugins.gulpif(global.isWatching, plugins.cached('scss')))
+        //
+        ////find files that depend on the files that have changed
+        //.pipe(plugins.sassInheritance({dir: dirs.src + '/scss/'}))
+        //
+        ////filter out internal imports (folders and files starting with "_" )
+        //.pipe(plugins.filter(function (file) {
+        //    return !/\/_/.test(file.path) || !/^_/.test(file.relative);
+        //}))
+        .pipe(plugins.debug({title: config.debug.title}))
+        .pipe(plugins.changed(DEST, {
+            hasChanged: plugins.changed.compareSha1Digest,
+            extension: '.css'
+        }))
         .pipe(plugins.sass({
             includePaths: bourbon.includePaths
-        }))
+        }).on('error', plugins.sass.logError))
         .pipe(plugins.autoprefixer([
             'last 3 versions',
             '> 1%',
@@ -161,26 +177,51 @@ gulp.task('build:sass', function () {
             cascade: true
         }))
         .pipe(plugins.cssbeautify())
-        .pipe(plugins.size())
-        .pipe(plugins.changed(DEST, {hasChanged: plugins.changed.compareSha1Digest, extension: '.css'}))
-        .pipe(plugins.debug({title: config.debug.title}))
-        .pipe(gulp.dest(DEST))
-        .pipe(plugins.rename({suffix: '.min'}))
-        .pipe(plugins.sass({outputStyle: 'compressed'}))
         .pipe(plugins.debug({title: config.debug.title}))
         .pipe(plugins.size())
+        .pipe(plugins.debug({title: config.debug.title}))
         .pipe(plugins.sourcemaps.write('./'))
-        .pipe(gulp.dest(DEST))
-        .pipe(livereload());
+        .pipe(gulp.dest(DEST));
+    // .pipe(livereload());
+});
+
+
+gulp.task('minify:sass', function () {
+    var SRC = dirs.src + '/scss/*.s+(a|c)ss';
+    var DEST = dirs.dist + '/css/';
+
+    return gulp.src(SRC)
+        .pipe(plugins.sourcemaps.init())
+        .pipe(plugins.sassGlob())
+        .pipe(plugins.sass({
+            includePaths: bourbon.includePaths,
+            outputStyle: 'compressed'
+        }).on('error', plugins.sass.logError))
+
+        .pipe(plugins.autoprefixer([
+            'last 3 versions',
+            '> 1%',
+            'ie >= 7'
+        ], {
+            cascade: true
+        }))
+        .pipe(plugins.changed(DEST, {hasChanged: plugins.changed.compareSha1Digest, extension: '.min.css'}))
+        .pipe(plugins.size())
+        .pipe(plugins.debug({title: config.debug.title}))
+        .pipe(plugins.rename({suffix: '.min'}))
+        .pipe(plugins.sourcemaps.write('./'))
+        .pipe(gulp.dest(DEST));
+
+    // .pipe(livereload());
 });
 
 /**
  * Watch all changes
  */
 gulp.task('watch', function () {
-    livereload.listen();
+    //livereload.listen();
     gulp.watch(dirs.src + '/js/**/*.js', ['lint:js', 'build:js']);
-    gulp.watch(dirs.src + '/scss/**/*.s+(a|c)ss', ['lint:sass', 'build:sass']);
+    gulp.watch(dirs.src + '/scss/**/*.s+(a|c)ss', ['setWatch', 'build:sass']);   //'lint:sass'
     gulp.watch(dirs.src + '/**/*.html', ['build:html']);
     gulp.watch(dirs.src + '/img/**', ['copy:img']);
 });
